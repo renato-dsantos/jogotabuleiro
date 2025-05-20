6c0b32f352e2c91998d96de50872685626bd9346
RENATO DOS SANTOS <renato.santos25@senacsp.edu.br>
2025-05-19 22:04:34 -0300

    novo

--- jogoCampo/src/Main.java
+++ jogoCampo/src/Main.java
@@ -1,9 +1,25 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int [][] campo = new int[][]{{1, -2, 3}, {4, -5, 6}, {-7, 8, 9}};
+
+        for(int linha = 0; linha<campo.length;linha++){
+            for(int  coluna = 0; coluna<campo[linha].length;coluna++){
+
+
+
+
+
+            }
+        }//fim for
 
         }
-    }
+
 }
