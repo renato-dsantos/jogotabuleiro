aa0284be8848a5ae21250a440e48cf6c8cbf3504
RENATO DOS SANTOS <renato.santos25@senacsp.edu.br>
2025-05-21 19:14:52 -0300

    atualizado

--- jogoCampo/src/Main.java
+++ jogoCampo/src/Main.java
@@ -1,25 +1,128 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
-
-public class Main {
-
-
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int [][] campo = new int[][]{{1, -2, 3}, {4, -5, 6}, {-7, 8, 9}};
-
-        for(int linha = 0; linha<campo.length;linha++){
-            for(int  coluna = 0; coluna<campo[linha].length;coluna++){
-
-
-
-
-
-            }
-        }//fim for
-
-        }
-
+import java.util.Random;
+public class Main
+{
+	public static void main(String[] args) {
+	   //Scanner 
+	    Scanner input = new Scanner(System.in);
+	    //random
+        Random sorteio = new Random();	   
+	   //Variáveis do jogador, recebe a linah e coluna do jogador
+	    int escolheLinha = 0;
+	    int escolheColuna = 0;
+	    int linhaMinas=0;
+	    int colunaMinas=0;
+	    
+	    //inicia o a matriz Campo que será mostrador
+	        int [][] campoExibir = new int [5][5];
+	        int [][] campoMinas = new int [5][5];
+	        
+	          
+	          ///testar minas.
+	        
+	       
+	      // System.out.println("Minas|__________");
+		    for(linhaMinas=0; linhaMinas<campoMinas.length;linhaMinas++){
+		   // System.out.print(linhaMinas + "     | ");
+		    for(colunaMinas=0;colunaMinas<campoMinas[linhaMinas].length;colunaMinas++){
+		        
+		        campoMinas[linhaMinas][colunaMinas] = sorteio.nextInt(4) + 1;
+		        
+		        
+		      //  System.out.print(campoMinas[linhaMinas][colunaMinas] + " ");
+		    }
+		   // System.out.println(" ");
+		}
+        	//	System.out.println("        _________        ");
+        	//	System.out.println("        0 1 2 3 4 Colunas");
+	        
+	        
+	        
+	        
+	    
+	       //for que exibe a matriz
+	       System.out.println("**-----Campo Minado-----**");
+	       System.out.println("**Escolha a linha e a coluna.**");
+	       System.out.println("**Cuidado para não acertar uma mina.**");
+	       
+	       System.out.println("Linhas|__________");
+		    for(int linha=0; linha<campoExibir.length;linha++){
+		    System.out.print(linha + "     | ");
+		    for(int coluna=0;coluna<campoExibir[linha].length;coluna++){
+		        System.out.print(campoExibir[linha][coluna] + " ");
+		    }
+		    System.out.println(" ");
+		}
+        		System.out.println("        _________        ");
+        		System.out.println("        0 1 2 3 4 Colunas");
+        		
+        		
+	    
+	    //Laço de repetição para mostrar o campo minado e atribuir valores
+	do{
+	            
+	            
+	            System.out.println("Informe a linha: ");
+		        escolheLinha = input.nextInt();
+		        System.out.println("Informe a coluna: ");
+		        escolheColuna = input.nextInt();
+		        
+		                  //verifica se o jogador acertoua bom
+        		            if(campoMinas[escolheLinha][escolheColuna] == 4){
+        		                
+        		                System.out.println("Bumm!");
+        		                System.out.println("Você acertou a Mina!");
+        		                System.out.println("Lina|__________");
+		                        for(linhaMinas=0; linhaMinas<campoMinas.length;linhaMinas++){
+		                       System.out.print(linhaMinas + "     | ");
+		                        for(colunaMinas=0;colunaMinas<campoMinas[linhaMinas].length;colunaMinas++){
+		                       System.out.print(campoMinas[linhaMinas][colunaMinas] + " ");
+		                          }
+		                        System.out.println(" ");
+	                            	}
+                                	System.out.println("        _________        ");
+                                    System.out.println("        0 1 2 3 4 Colunas");
+        		                break;
+        		                
+        		            }else{
+        		               campoMinas[escolheLinha][escolheColuna] = 1;
+        		               // System.out.println("Minas|__________");
+		                        for(linhaMinas=0; linhaMinas<campoMinas.length;linhaMinas++){
+		                       // System.out.print(linhaMinas + "     | ");
+		                        for(colunaMinas=0;colunaMinas<campoMinas[linhaMinas].length;colunaMinas++){
+		                       // System.out.print(campoMinas[linhaMinas][colunaMinas] + " ");
+		                            }
+		                            // System.out.println(" ");
+	                            	}
+        	                        //	System.out.println("        _________        ");
+        	                        //	System.out.println("        0 1 2 3 4 Colunas");
+        		                  }//fim if
+	    
+	    
+	    
+		                    System.out.println("Linhas|__________");
+	                    	//for que exibe a matriz e adicona o numero 1 no campo de exibição
+		                    for(int linha=0; linha<campoExibir.length;linha++){
+		                    System.out.print(linha + "     | ");
+		                    for(int coluna=0;coluna<campoExibir[linha].length;coluna++){
+		        
+		                    System.out.print(campoExibir[linha][coluna] + " ");
+		                    campoExibir[escolheLinha][escolheColuna] = 1;
+		        
+		                    }
+		                    System.out.println(" ");
+		    
+		    
+		                    }//fim for
+        		            System.out.println("        _________        ");
+        		            System.out.println("        0 1 2 3 4 Colunas");
+        		            
+        		            
+        		
+	}while(true);
+	    
+		
+	}
 }
